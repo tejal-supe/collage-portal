319433e2c81b59f7f7edb6bdb454957a7e32a8c5
tejal-supe <supetejalc@gmail.com>
2025-09-16 18:49:31 +0530

    db connection and environment config setup

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 import app from "./src/app.js";
+import { connectDB } from "./src/config/db.js";
+import config from "./src/config/environment.js";
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.listen(config.port, () =>
+  console.log("Server running on port", config.port)
+);
+connectDB();
