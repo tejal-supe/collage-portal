6c23dc49fb82087c7243aab524c9d8718be9241f
tejal-supe <supetejalc@gmail.com>
2025-09-18 13:26:04 +0530

    User controllers

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,93 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+
+import User from "../models/user.model.js";
+import config from "../config/environment.js";
+
+
+export const registerUserController = async(req,res)=>{
+    try {
+      const { firstName, lastName, emailId, password } = req.body;
+      if (!firstName || !lastName || !emailId || !password) {
+        return res.status(400).json({ message: "Enter all the User details" });
+      }
+      const exisitingUser = await User.findOne({ emailId });
+      if (exisitingUser) {
+        return res.status(400).json({ message: "User Already Exists" });
+      }
+      const newUser = await User.create({
+        firstName,
+        lastName,
+        emailId,
+        password,
+      });
+      if (!newUser) {
+        return res.status(400).json({ message: "Error while creating a user" });
+      }
+      res.status(201).json({
+        message: "User Registered Successfully!",
+        success: true,
+      });
+    } catch (error) {
+        res.status(500).json({message:"Internal Server Error"})
+      console.log("Error in the register user controller", error);
+    }
+}
+
+export const loginUserController  = async(req,res)=>{
+     try {
+       const { emailId, password } = req.body;
+       if (!emailId || !password) {
+         return res.status(400).json({ message: "Please Enter the details" });
+       }
+       const userData = await User.findOne({ emailId });
+       if (!userData) {
+         return res
+           .status(400)
+           .json({ message: "The Entered EmailId or Password doesn't exist" });
+       }
+       const passwordMatch = await bcrypt.compare(password, userData.password);
+       if (!passwordMatch) {
+         return res.status(400).json({
+           message: "The Entered EmailId or Password doesn't exist",
+         });
+       }
+       const token = jwt.sign({ id: userData._id }, config.jwt_secret, {
+         expiresIn: config.jwt_expiry,
+       });
+
+       const cookieOptions = {
+         httpOnly: true,
+         secure: true,
+         maxAge: 24 * 60 * 60 * 1000,
+       };
+       res.cookie("token", token, cookieOptions);
+       res.status(201).json({
+         message: "User logged in succesfully",
+         success: true,
+         user: {
+           id: userData._id,
+           firstName: userData.firstName,
+           lastName: userData.lastName,
+         },
+       });
+     } catch (error) {
+       console.log("Error in Login Controller", error);
+     }
+}
+
+export const getMeController = async (req, res) => {
+  try {
+    const { _id } = req.user;
+
+    const userData = await User.findById({ _id }).select("-password");
+    if (!userData) {
+      return res.status(400).json({ message: "User data doesn't exist" });
+    }
+    return res
+      .status(201)
+      .json({ message: "User data fetched successfully", data: userData });
+  } catch (error) {
+    console.log("Error in get me controller", error);
+  }
+};
